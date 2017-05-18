1a5bd6daecde77d585062800c447a85f3619fbbb
Asim Khawaja <asimn@hotmail.com>
2017-05-18 12:09:59 +1000

    added new adhan for fair

--- adhanPlayer.py
+++ adhanPlayer.py
@@ -2,9 +2,31 @@ import datetime
 import time
 import requests
 import pygame
+import os
+import random
 
 
-       
+
+def GetRandomIndexForMusicFile (filesArray):
+      randomFileIndexFromFilesArray =  random.randint(0,len(filesArray) -1 )
+      if  filesArray[randomFileIndexFromFilesArray] == ".":     
+            #this is a hidden file , skip it and find a new index
+             GetRandomIndexForMusicFile(filesArray)
+      else:
+          #valid music file
+          return  filesArray[randomFileIndexFromFilesArray]     
+
+def GetAdhanFile(prayer):
+      fileName =""
+      if prayer =="FAJR":
+         for root, dirs, files in os.walk('media/fajr',topdown=True):
+                  fileName = GetRandomIndexForMusicFile(files)
+                  return fileName
+      else:
+            #pick a random mp3 from the folder
+            for root, dirs, files in os.walk('media',topdown=True):
+                  fileName = GetRandomIndexForMusicFile(files)
+                  return fileName              
 
 
 def GetLatestPrayerTimes():
@@ -44,16 +66,19 @@ while True:
                #timestamp is valid now to check prayer timings
                print "checking prayer timing match"
                for prayer in prayerTimes :
+                     
                      prayerHour = (int)(prayerTimes[prayer].split(':')[0])
                      if prayerHour == currentTime.hour and adhanPlayed[prayer] ==False: 
                            #found a prayer with the matching hour:
-                           print "potential time for "  , prayer
+                           print "found matching hour for "  , prayer
                            #now check if its actually prayer time or within a few minutes of it?
                            prayerMinutes  = (int)(prayerTimes[prayer].split(':')[1])
                            if  prayerMinutes - currentTime.minute <=0 and prayerMinutes - currentTime.minute >= -2:
                                   #play adhan
+                                  print "playing adhan for " , prayer
                                   adhanPlayed[prayer] =True
                                   pygame.mixer.init()
+                                  fileName = GetAdhanFile(prayer)
                                   pygame.mixer.music.load("media/a1.mp3")
                                   pygame.mixer.music.play()
                                   while pygame.mixer.music.get_busy() == True:
@@ -61,11 +86,11 @@ while True:
                                   pass
                      else:
                            #adhan has already prayer for this prayer
-                           print "No prayer times sleeping 30 seconds"
-                           time.sleep(30)
                            pass
+               print "No prayer time match has occured. Sleeping 30 seconds"
+               time.sleep(30)   
       else:
-            print " no prayer timeings so far"
+            print " no prayer timing so far"
             timestamp, prayerTimes, adhanPlayed =GetLatestPrayerTimes()
             print "Prayer Timings were refreshed on: " , timestamp 
             
